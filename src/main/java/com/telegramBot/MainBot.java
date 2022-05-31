6543e3c5628b68d6ad35833fbe4fc2f92e4d9d08
Dev1NA <andreykoandroshchuk@gmail.com>
2022-05-31 22:07:05 +0300

    Save username to db

--- src/main/java/com/telegramBot/MainBot.java
+++ src/main/java/com/telegramBot/MainBot.java
@@ -1,14 +1,29 @@
 package com.telegramBot;
 
+import com.telegramBot.repo.Repo;
+import com.telegramBot.repo.UserFromTelegram;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Component
 public class MainBot extends TelegramLongPollingBot {
+
+    private Repo repo;
+
+    @Autowired
+    public void initRepo(Repo repo) {
+        this.repo = repo;
+    }
 
     @Value("${bot.username}")
     private String username;
@@ -23,10 +38,11 @@ public class MainBot extends TelegramLongPollingBot {
 
             SendMessage sm = new SendMessage();
             sm.setChatId(chatId);
-            sm.setText("Hello, " + update.getMessage().getFrom().getFirstName() + "!");
-
+            sm.setText("Hello, bro-" + update.getMessage().getFrom().getFirstName() + "!");
+            repo.save(new UserFromTelegram(update.getMessage().getFrom().getFirstName()));
             try {
                 execute(sm);
+
             } catch (TelegramApiException e) {
                 e.printStackTrace();
             }
@@ -43,5 +59,21 @@ public class MainBot extends TelegramLongPollingBot {
         return token;
     }
 
+//    public static InlineKeyboardMarkup sendInlineKeyBoardMessage(String chatId) {
+//        InlineKeyboardMarkup inlineKeyboardMarkup = new InlineKeyboardMarkup();
+//        InlineKeyboardButton inlineKeyboardButton1 = new InlineKeyboardButton();
+//        inlineKeyboardButton1.setText("Save user");
+//        inlineKeyboardButton1.setCallbackData("Button \"Save user\" has been pressed");
+//
+//        List<List<InlineKeyboardButton>> rowList = new ArrayList<>();
+//        List<InlineKeyboardButton> keyboardButtonsRow1 = new ArrayList<>();
+//        keyboardButtonsRow1.add(inlineKeyboardButton1);
+//        rowList.add(keyboardButtonsRow1);
+//
+//        inlineKeyboardMarkup.setKeyboard(rowList);
+//
+//        return inlineKeyboardMarkup;
+//
+//    }
 
 }
--- src/main/java/com/telegramBot/TelegramBotApplication.java
+++ src/main/java/com/telegramBot/TelegramBotApplication.java
@@ -1,11 +1,12 @@
 package com.telegramBot;
 
+import com.telegramBot.repo.Repo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TelegramBotApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(TelegramBotApplication.class, args);
 	}
